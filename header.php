397d75a92fd27c26f783d549d6c7dcec87d3dfa9
Billy <billymcfly@gmail.com>
2018-05-05 20:54:32 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<?php wp_head(); ?>
+</head>
+<body>
+	<nav id="topnav">
+		<a href="#about">About Me</a>
+		<a href="#skills">Skills</a>
+		<a href="#projects">Projects</a>
+		<a href="#blog">Blog</a>
+		<a href="#contact">Contact</a>
+	</nav>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+get_header(); ?>
+
+	<section id="hero">
+		William McPherson
+	</section>
+
+	<section id="about">
+		<h1>About Me</h1>
+			My name is William McPherson, but everyone knows me as Billy. I have studied at <a href="http://www.westwood.edu/" target="_blank">Westwood College</a> since January 2011 and graduated in July of 2015, Bachelor's of Science in Game Software Developement program. Currently I am a QA engineer at <a href="https://level10qa.com" target="_blank">Level 10 QA</a>.
+	</section>
+
+	<section id="skills">
+		<h1 id="skills">Skills</h1>
+		You'll find my skills pretty varied, but here's some of what I can do and/or have experience with: <br />
+		<ul class="skillslist">
+			<li>HTML5</li>
+			<li>CSS3</li>
+			<li>PHP5</li>
+			<li>MySQL</li>
+			<li>Javascript</li>
+			<li>JQuery</li>
+		</ul>
+		<ul class="skillslist">
+			<li>C/C++</li>
+			<li>Limited OpenGL experience</li>
+			<li>Limited DirectX experience</li>
+			<li>Memory management (heap and stack)</li>
+			<li>Standard template library</li>
+			<li>Boost library</li>
+		</ul>
+		<ul class="skillslist">
+			<li>C# .NET</li>
+			<li>WinForms</li>
+			<li>Microsoft SQL</li>
+			<li>Common Language Runtime</li>
+			<li>Unity Engine Scripting</li>
+		</ul>
+		<ul class="skillslist">
+			<li>Software testing practices</li>
+			<li>Writing a test plan</li>
+			<li>Black/white box testing</li>
+			<li>Unit testing</li>
+			<li>Ad-hoc testing</li>
+		</ul>
+		<ul class="skillslist">
+			<li>Computer assembly and repair</li>
+			<li>Some networking (port forwarding, etc)</li>
+			<li>LAMP server setup/hosting</li>
+			<li>GNU/Linux installation and administration</li>
+			<li>Windows installation and administration</li>
+		</ul>
+	</section>
+	<div class="angledcomponent"></div>
+
+	<section id="projects">
+		<h2>Projects</h2>
+		<p>Here are things I have done in the past (both in-school and out).</p>
+		
+		<img src="/portfolio/img/bazoik_screenshot.jpg" alt="" style="float: left">
+		
+		<img src="/portfolio/img/vgcatalog_screenshot.png" alt="" style="float: left">
+		
+		<img src="/portfolio/img/blackrabbit_screenshot.png" alt="" style="float: left">
+		
+		<img src="/portfolio/img/dxm01_screenshot.png" alt="" style="float: left">
+		
+		<img src="/portfolio/img/combat_screenshot.png" alt="" style="float: left">
+		
+		<img src="/portfolio/img/rotw_screenshot.png" alt="" style="float: left">
+		
+		<img src="/portfolio/img/demonoid_screenshot.png" alt="" style="float: left">
+		
+        <img src="/portfolio/img/philosophers_screenshot.png" width="150" alt="" style="float: left">
+        
+		<img src="/portfolio/img/tictactoe_screenshot.png" alt="" style="float: left">
+	</section>
+
+<?php get_footer(); ?>
